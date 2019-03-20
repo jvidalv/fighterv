d3fa394863736bba4af7548bd5cbce21689611bc
jvidalv <josepvidalvidal@gmail.com>
2019-03-20 15:21:35 +0100

    Updates

---
+++ precache-manifest.ff3bf1b99e97e80e40f907eadf4c4a99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a25191ee9afd12212cf51feb6b8e42a7",
+    "url": "/fighterv/static/media/map.a25191ee.png"
+  },
+  {
+    "revision": "b270133277a720d607c6b307305694eb",
+    "url": "/fighterv/static/media/goblin_camp.b2701332.png"
+  },
+  {
+    "revision": "181de86513006d771c8ade0663672d62",
+    "url": "/fighterv/static/media/bg2.181de865.jpg"
+  },
+  {
+    "revision": "5bdc176220c4e0430c54",
+    "url": "/fighterv/static/js/runtime~main.625aac23.js"
+  },
+  {
+    "revision": "b3e21bd477197a6125bf",
+    "url": "/fighterv/static/js/main.fd2cad2e.chunk.js"
+  },
+  {
+    "revision": "00a297d1fd308a344a61",
+    "url": "/fighterv/static/js/2.c0fe7a12.chunk.js"
+  },
+  {
+    "revision": "b3e21bd477197a6125bf",
+    "url": "/fighterv/static/css/main.bbdb1ef8.chunk.css"
+  },
+  {
+    "revision": "1d12b9efc0020494a68b917f15f0cde0",
+    "url": "/fighterv/index.html"
+  }
+];
