b4f85586d76b76cdaa9deb2cd43865e7bac35b89
Josep Vidal <josepvidalvidal@gmail.com>
2020-01-17 21:04:19 +0100

    Updates

---
+++ precache-manifest.ce1b12d4c6dc883eff0c46f2227da3e9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9367e02788fa9bfc2cb0ba4f95f5db16",
+    "url": "/react-old-fighterv/index.html"
+  },
+  {
+    "revision": "c4e3a11b892ea8997334",
+    "url": "/react-old-fighterv/static/css/main.54e0f5aa.chunk.css"
+  },
+  {
+    "revision": "feb17768655ada9654e8",
+    "url": "/react-old-fighterv/static/js/2.6235f702.chunk.js"
+  },
+  {
+    "revision": "0bca0224b6af2a656dcc680de3c649da",
+    "url": "/react-old-fighterv/static/js/2.6235f702.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4e3a11b892ea8997334",
+    "url": "/react-old-fighterv/static/js/main.d6cb443d.chunk.js"
+  },
+  {
+    "revision": "f0a7431f2c0256c95568",
+    "url": "/react-old-fighterv/static/js/runtime-main.0fa1e1e5.js"
+  },
+  {
+    "revision": "181de86513006d771c8ade0663672d62",
+    "url": "/react-old-fighterv/static/media/bg2.181de865.jpg"
+  },
+  {
+    "revision": "8847dfc1bdb8f537d77f32e0f5e0700f",
+    "url": "/react-old-fighterv/static/media/elf_camp.8847dfc1.png"
+  },
+  {
+    "revision": "b270133277a720d607c6b307305694eb",
+    "url": "/react-old-fighterv/static/media/goblin_camp.b2701332.png"
+  },
+  {
+    "revision": "99f2a6f89268a3086e5d1e6a6ece9dd3",
+    "url": "/react-old-fighterv/static/media/logo.99f2a6f8.png"
+  },
+  {
+    "revision": "a25191ee9afd12212cf51feb6b8e42a7",
+    "url": "/react-old-fighterv/static/media/map.a25191ee.png"
+  },
+  {
+    "revision": "61b65945184f65fa6fe3db1d38497312",
+    "url": "/react-old-fighterv/static/media/plank-old.61b65945.png"
+  },
+  {
+    "revision": "d1e08af5f4838985ed2dcdb8f2a50521",
+    "url": "/react-old-fighterv/static/media/wood-plan-l.d1e08af5.png"
+  },
+  {
+    "revision": "87185c3eec71b3df8bf20e60c60ae79b",
+    "url": "/react-old-fighterv/static/media/wood-plank-r.87185c3e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fighterv/precache-manifest.7944f21461feb283c2356ad18f57e49b.js"
+  "/react-old-fighterv/precache-manifest.ce1b12d4c6dc883eff0c46f2227da3e9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/fighterv/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-old-fighterv/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
